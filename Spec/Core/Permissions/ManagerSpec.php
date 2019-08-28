12402bb2d85fe3b4e0e3968de669ec96bbc05e7d
Guy Thouret <guy@minds.com>
2019-08-28 15:58:36 -0400

    (fix) Regeressions in existing spec tests - #616

--- Spec/Core/Permissions/ManagerSpec.php
+++ Spec/Core/Permissions/ManagerSpec.php
@@ -2,12 +2,11 @@
 
 namespace Spec\Minds\Core\Permissions;
 
+use Minds\Core\Entities\PropagateProperties;
 use Minds\Core\Permissions\Manager;
 use Minds\Core\Permissions\Permissions;
 use Minds\Entities\User;
 use PhpSpec\ObjectBehavior;
-use Minds\Core\EntitiesBuilder;
-use Minds\Core\Data\Call;
 use Minds\Core\Entities\Actions\Save;
 use Minds\Entities\Entity;
 use Minds\Entities\Image;
@@ -16,22 +15,18 @@ class ManagerSpec extends ObjectBehavior
 {
     /** @var User */
     protected $user;
-    /** @var EntitiesBuilder */
-    protected $entitiesBuilder;
-    /** @var Call */
-    protected $db;
     /** @var Save */
     protected $save;
+    /** @var PropagateProperties */
+    protected $propagateProperties;
 
     public function let(
-        EntitiesBuilder $entitiesBuilder,
-        Call $db,
-        Save $save
+        Save $save,
+        PropagateProperties $propagateProperties
     ) {
-        $this->entitiesBuilder = $entitiesBuilder;
-        $this->db = $db;
         $this->save = $save;
-        $this->beConstructedWith($this->entitiesBuilder, $this->db, $this->save);
+        $this->propagateProperties = $propagateProperties;
+        $this->beConstructedWith($this->save, $this->propagateProperties);
     }
 
     public function it_is_initializable()
@@ -43,46 +38,9 @@ class ManagerSpec extends ObjectBehavior
     {
         $permissions = new Permissions();
         $entity->setAllowComments(true)->shouldBeCalled();
-        $entity->get('entity_guid')->shouldBeCalled()->willReturn(false);
-        $entity->getGUID()->shouldBeCalled()->willReturn(1);
-        $entity->getType()->shouldBeCalled()->willReturn('activity');
-        $this->db->getRow('activity:entitylink:1')->shouldBeCalled()->willReturn([]);
         $this->save->setEntity($entity)->shouldBeCalled()->willReturn($this->save);
         $this->save->save()->shouldBeCalled();
-        $this->save($entity, $permissions);
-    }
-
-    public function it_should_save_attachment_permissions(Entity $entity, Image $image)
-    {
-        $permissions = new Permissions();
-        $image->setAllowComments(true)->shouldBeCalled();
-        $this->entitiesBuilder->single(1)->shouldBeCalled()->willReturn($image);
-        $entity->setAllowComments(true)->shouldBeCalled();
-        $entity->getGUID()->shouldBeCalled()->willReturn(1);
-        $entity->getType()->shouldBeCalled()->willReturn('activity');
-        $entity->get('entity_guid')->shouldBeCalled()->willReturn(1);
-        $this->db->getRow('activity:entitylink:1')->shouldBeCalled()->willReturn([]);
-        $this->save->setEntity($entity)->shouldBeCalled()->willReturn($this->save);
-        $this->save->setEntity($image)->shouldBeCalled()->willReturn($this->save);
-        $this->save->save()->shouldBeCalled();
-        $this->save($entity, $permissions);
-    }
-
-    public function it_should_save_linked_entity_permissions(Entity $entity, Entity $parent)
-    {
-        $permissions = new Permissions();
-        $parent->setAllowComments(true)->shouldBeCalled();
-        $this->db->getRow('activity:entitylink:1')->shouldBeCalled()
-            ->willReturn([2 => $parent]);
-        $this->entitiesBuilder->single(1)->shouldBeCalled()->willReturn($parent);
-        $entity->setAllowComments(true)->shouldBeCalled();
-        $entity->getGUID()->shouldBeCalled()->willReturn(1);
-        $entity->getType()->shouldBeCalled()->willReturn('activity');
-        $entity->get('entity_guid')->shouldBeCalled()->willReturn(1);
-        $this->db->getRow('activity:entitylink:1')->shouldBeCalled()->willReturn([]);
-        $this->save->setEntity($entity)->shouldBeCalled()->willReturn($this->save);
-        $this->save->setEntity($parent)->shouldBeCalled()->willReturn($this->save);
-        $this->save->save()->shouldBeCalled();
+        $this->propagateProperties->from($entity)->shouldBeCalled();
         $this->save($entity, $permissions);
     }
 }
