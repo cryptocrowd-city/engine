8e3fcffe717bd1b4f67e0221bff128af089f113a
Guy Thouret <guy@minds.com>
2019-08-29 17:07:26 -0400

    (chore) Add doc blocks for Propagator\Properties - #616

--- Core/Entities/Propagator/Properties.php
+++ Core/Entities/Propagator/Properties.php
@@ -4,22 +4,45 @@ namespace Minds\Core\Entities\Propagator;
 
 use Minds\Entities\Activity;
 
+/**
+ * Properties class that all PropagateProperties delegates should inherit
+ * @package Minds\Core\Entities\Propagator
+ */
 abstract class Properties
 {
+    /**
+     * @var string
+     */
     protected $actsOnType = 'any';
+    /**
+     * @var string
+     */
     protected $actsOnSubtype = 'any';
+    /**
+     * @var bool
+     */
     protected $changed = false;
 
+    /**
+     * @return string
+     */
     public function actsOnType(): string
     {
         return $this->actsOnType;
     }
 
+    /**
+     * @return string
+     */
     public function actsOnSubType(): string
     {
         return $this->actsOnSubtype;
     }
 
+    /**
+     * @param $entity
+     * @return bool
+     */
     public function willActOnEntity($entity): bool
     {
         if ($this->actsOnType === 'any' || $this->actsOnType === $entity->getType()) {
@@ -29,6 +52,11 @@ abstract class Properties
         return false;
     }
 
+    /**
+     * @param $from
+     * @param $to
+     * @return bool
+     */
     protected function valueHasChanged($from, $to): bool
     {
         $changed = $from !== $to;
@@ -36,6 +64,9 @@ abstract class Properties
         return $changed;
     }
 
+    /**
+     * @return bool
+     */
     public function changed(): bool
     {
         return $this->changed;
