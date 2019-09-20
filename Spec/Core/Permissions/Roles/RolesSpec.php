a0695d01ae942580d0d8c66289ed82868f412948
Brian Hatchet <brian@minds.com>
2019-09-20 09:25:18 -0600

    Testing banned role

---
+++ Spec/Core/Permissions/Roles/RolesSpec.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Spec\Minds\Core\Permissions\Roles;
+
+use Minds\Core\Permissions\Roles\Roles;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RolesSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Roles::class);
+    }
+
+    public function it_should_have_admin_permissions()
+    {
+        $role = $this->getRole(Roles::ROLE_ADMIN);
+        $role->hasPermission(Roles::FLAG_APPOINT_ADMIN)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_CREATE_POST)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_EDIT_CHANNEL)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_EDIT_POST)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_DELETE_CHANNEL)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_DELETE_POST)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_APPOINT_MODERATOR)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_APPROVE_SUBSCRIBER)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_SUBSCRIBE)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_VIEW)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_VOTE)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_CREATE_COMMENT)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_EDIT_COMMENT)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_DELETE_COMMENT)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_REMIND)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_WIRE)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_MESSAGE)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_INVITE)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_CREATE_GROUP)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_EDIT_GROUP)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_DELETE_GROUP)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_JOIN)->shouldEqual(true);
+        $role->hasPermission(Roles::FLAG_JOIN_GATHERING)->shouldEqual(true);
+    }
+
+    public function it_should_have_banned_permissions()
+    {
+        $role = $this->getRole(Roles::ROLE_BANNED);
+        $role->hasPermission(Roles::FLAG_APPOINT_ADMIN)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_CREATE_POST)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_EDIT_CHANNEL)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_EDIT_POST)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_DELETE_CHANNEL)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_DELETE_POST)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_APPOINT_MODERATOR)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_APPROVE_SUBSCRIBER)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_SUBSCRIBE)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_VIEW)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_VOTE)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_CREATE_COMMENT)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_EDIT_COMMENT)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_DELETE_COMMENT)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_REMIND)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_WIRE)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_MESSAGE)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_INVITE)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_CREATE_GROUP)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_EDIT_GROUP)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_DELETE_GROUP)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_JOIN)->shouldEqual(false);
+        $role->hasPermission(Roles::FLAG_JOIN_GATHERING)->shouldEqual(false);
+    }
+}
