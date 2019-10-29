6ce01682d08946325b509fc17425ee5f0ddb6641
Guy Thouret <guy@minds.com>
2019-10-29 16:23:47 +0000

    (chore) Remove braintree package and referencing code; Replace abandoned Facebook package dependency - #1066

--- Core/Payments/Factory.php
+++ Core/Payments/Factory.php
@@ -9,8 +9,6 @@ class Factory
     public static function build($handler, $opts = [])
     {
         switch (ucfirst($handler)) {
-          case "Braintree":
-            return Di::_()->get('BraintreePayments')->setConfig($opts);
           default:
             throw new \Exception("Service not found");
         }
