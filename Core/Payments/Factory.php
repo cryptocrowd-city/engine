2035b9b4b8164ef484aeb8fe1282749f25a45c9c
Guy Thouret <guy@minds.com>
2019-10-29 10:41:08 +0000

    (chore) Payments\Factory - Remove inaccurate docblocks - #1066

--- Core/Payments/Factory.php
+++ Core/Payments/Factory.php
@@ -1,22 +1,11 @@
 <?php
-/**
-* Payments Factory
-*/
+
 namespace Minds\Core\Payments;
 
 use Minds\Core\Di\Di;
 
-/**
- * A factory providing handlers boosting items
- */
 class Factory
 {
-    /**
-     * Build the handler
-     * @param string $handler
-     * @param array $options (optional)
-     * @return BoostHandlerInterface
-     */
     public static function build($handler, $opts = [])
     {
         switch (ucfirst($handler)) {
