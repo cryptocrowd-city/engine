a0b8ed4757346cd7dc471bd8249f010cec2503c2
Marcelo Rivera <malrogf@gmail.com>
2019-12-19 09:40:56 -0300

    (fix): update feature flags

--- settings.example.php
+++ settings.example.php
@@ -482,9 +482,9 @@ $CONFIG->set('features', [
     'pro' => false,
     'webtorrent' => false,
     'top-feeds-by-age' => true,
-    'homepage' => true,
-    'onboarding' => true,
-    'register_pages' => true,
+    'homepage-december-2019' => true,
+    'onboarding-december-2019' => true,
+    'register_pages-december-2019' => true,
 ]);
 
 $CONFIG->set('email', [
