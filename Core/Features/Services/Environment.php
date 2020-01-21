2b27c12869339a5b50d658c95d25de3737fce3a9
Emiliano Balbuena <edgebal@gmail.com>
2020-01-21 19:31:27 -0300

    (feat): Environment variables service

--- Core/Features/Services/Environment.php
+++ Core/Features/Services/Environment.php
@@ -7,6 +7,10 @@
 
 namespace Minds\Core\Features\Services;
 
+/**
+ * Environment variables ($_ENV) feature flags service
+ * @package Minds\Core\Features\Services
+ */
 class Environment extends BaseService
 {
     /**
@@ -14,6 +18,31 @@ class Environment extends BaseService
      */
     public function fetch(array $keys): array
     {
-        return [];
+        $output = [];
+
+        foreach ($keys as $key) {
+            // Convert to variable name
+            // Example: `webtorrent` would be `MINDS_FEATURE_WEBTORRENT`; `psr7-router` would be `MINDS_FEATURE_PSR7_ROUTER`
+
+            $envName = sprintf('MINDS_FEATURE_%s', strtoupper(preg_replace('/[^a-zA-Z0-9]+/', '_', $key)));
+
+            if (isset($_ENV[$envName])) {
+                // Read value as string
+
+                $value = (string) $_ENV[$envName];
+
+                // Resolve group, if not 0 or 1
+
+                if (strlen($value) > 0 && $value !== '0' && $value !== '1') {
+                    $value = in_array(strtolower($value), $this->getUserGroups(), true);
+                }
+
+                // Set value
+
+                $output[$key] = (bool) $value;
+            }
+        }
+
+        return $output;
     }
 }
