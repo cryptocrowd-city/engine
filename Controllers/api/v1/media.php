fc74ee0e3661c85e85024606d070fc92cbf81855
Marcelo Rivera <malrogf@gmail.com>
2020-02-12 12:50:56 -0300

    (fix): set fullExport on v1/media

--- Controllers/api/v1/media.php
+++ Controllers/api/v1/media.php
@@ -43,6 +43,8 @@ class media implements Interfaces\Api, Interfaces\ApiIgnorePam
                 ]);
             }
 
+            $entity->fullExport = true;
+
             switch ($entity->subtype) {
                 case "video":
                     // Helpers\Counters::increment($pages[0], 'plays');
