f190b29c57e03c53cd3f22977a88f976dc915e3c
Marcelo Rivera <malrogf@gmail.com>
2020-03-30 13:58:54 -0300

    (feat): extend ObjectVideoMapping with new fields

--- Core/Search/Mappings/ObjectVideoMapping.php
+++ Core/Search/Mappings/ObjectVideoMapping.php
@@ -18,6 +18,8 @@ class ObjectVideoMapping extends EntityMapping implements MappingInterface
         $this->mappings = array_merge($this->mappings, [
             'license' => [ 'type' => 'text', '$exportField' => 'license' ],
             'rating' => [ 'type' => 'integer', '$exportField' => 'rating' ],
+            'youtube_id' => [ 'type' => 'string', '$exportField' => 'youtube_id' ],
+            'transcoding_status' => [ 'type' => 'string', '$exportField' => 'transcoding_status' ],
         ]);
     }
 }
