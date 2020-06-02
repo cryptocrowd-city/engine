e3da5ea5d4be1b7105254f08d003440a1bcf240b
Emi Balbuena <edgebal@gmail.com>
2020-06-02 20:05:48 -0300

    (cs): Code lint

--- Core/I18n/Manager.php
+++ Core/I18n/Manager.php
@@ -65,7 +65,7 @@ class Manager
             return strtolower($locale['code']);
         }, Locales::I18N_LOCALES);
 
-        return in_array($language, $localeCodes);
+        return in_array($language, $localeCodes, true);
     }
 
     /**
