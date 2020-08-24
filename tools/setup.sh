de5afa767c10a0efe78cd451d5df43031ec5d8f9
Ben Hayward <ben@minds.com>
2020-08-24 18:02:26 +0100

    Added link to get updated string

--- tools/setup.sh
+++ tools/setup.sh
@@ -12,6 +12,7 @@ fi
 rm -rf ../vendor
 
 # Setup composer
+# Hash update information - https://getcomposer.org/download/
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
