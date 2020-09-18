741f7d38e5c209d5fdc90636d5bf5ee8167f22aa
Ben Hayward <ben@minds.com>
2020-09-18 09:44:46 +0100

    Added some spec tests

--- Spec/Common/EntityMutationSpec.php
+++ Spec/Common/EntityMutationSpec.php
@@ -40,7 +40,7 @@ class EntityMutationSpec extends ObjectBehavior
         $diff['title']['mutated']->shouldBe('testing has changed');
     }
 
-    public function it_should_confirm_if_field_is_mutated()
+    public function it_should_confirm_if_message_field_is_mutated()
     {
         $this->activity->setTitle('testing');
         $this->activity->setMessage('my message will change');
@@ -51,6 +51,39 @@ class EntityMutationSpec extends ObjectBehavior
 
         $this->hasMutated('title')
             ->shouldBe(false);
+
+        $this->hasMutated('message')
+            ->shouldBe(true);
+    }
+
+
+    public function it_should_confirm_if_title_field_is_mutated()
+    {
+        $this->activity->setTitle('testing');
+        $this->activity->setMessage('my message will not change');
+
+        // Mutate
+        $this->setTitle('changed title');
+        $this->setMessage('my message will not change');
+
+        $this->hasMutated('title')
+            ->shouldBe(true);
+
+        $this->hasMutated('message')
+            ->shouldBe(false);
+    }
+
+    public function it_should_confirm_if_multiple_fields_are_mutated()
+    {
+        $this->activity->setTitle('testing title will change');
+        $this->activity->setMessage('my message will not change');
+
+        // Mutate
+        $this->setTitle('changed title');
+        $this->setMessage('my message changed');
+
+        $this->hasMutated('title')
+            ->shouldBe(true);
 
         $this->hasMutated('message')
             ->shouldBe(true);
