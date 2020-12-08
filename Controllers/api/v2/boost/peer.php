0182a363143f6fab98f54abff7aaa39895bc6a55
Juan Manuel Solaro <juanmsolaro@gmail.com>
2020-12-07 22:08:09 -0300

    (fix) use new remind system for boosts

--- Controllers/api/v2/boost/peer.php
+++ Controllers/api/v2/boost/peer.php
@@ -16,6 +16,7 @@ use Minds\Entities;
 use Minds\Interfaces;
 use Minds\Api\Factory;
 use Minds\Core\Payments;
+use Minds\Core\Feeds\Activity;
 
 class peer implements Interfaces\Api
 {
@@ -296,12 +297,14 @@ class peer implements Interfaces\Api
         $activity->ownerObj = Core\Session::getLoggedInUser()->export();
         $activity->p2p_boosted = true;
 
-        // TODO: How do we handle P2P boosts with remind refactor
-        // MH
         if ($embedded->remind_object) {
             $activity->setRemind($embedded->remind_object)->save();
         } else {
-            $activity->setRemind($embedded->export())->save();
+            $remindIntent = new Activity\RemindIntent();
+            $remindIntent->setGuid($boost->getEntity()->guid)
+                ->setOwnerGuid($boost->getOwner()->guid)
+                ->setQuotedPost(false));
+            $activity->setRemind($remindIntent)->save();
         }
 
         // Notify
